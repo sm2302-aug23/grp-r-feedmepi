903d5221a286743881ce5590f75faaf2c1409b0e
Sia Yee Zee <19b9013@ube.edu.bn>
2023-09-21 23:03:00 +0800

    Update 01-gen_collatz.R

--- 01-gen_collatz.R
+++ 01-gen_collatz.R
@@ -5,36 +5,6 @@ library(palmerpenguins)
 library(dplyr)
 library(tibble)
 
-
-# using if, else if and stop statements from lecture 1 -------------------------
-
-gen_collatz <- function(n) {
-  if (n > 0 && n %% 2 == 0) {
-    n / 2
-  } else if (n > 0 && n %% 2 != 0) {
-    3 * n + 1
-  } else if (n < 0) {
-    stop ("Input is negative")
-  }
-}
-
-# assigning integer 1 to 10000 into function(n) --------------------------------
-
-n <- c(1 : 10000)
-
-
-# creating collatz data frame --------------------------------------------------
-# sapply function cited from 
-#https://www.geeksforgeeks.org/apply-lapply-sapply-and-tapply-in-r/
-
-collatz_df <- tibble(data.frame(
-  start = n,
-  seq = sapply(n, gen_collatz)
- )
-)
-
-collatz_df
-
 #Aqil draft
 
 gen_collatz <- function(n) {
