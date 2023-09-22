5ecae2da49b5a291367b8943acb1986efde8d7c0
AqilMN <19b9002@ubd.edu.bn>
2023-09-22 20:09:36 +0800

    Update 03-backtracking.R
    updating function for backtrack

--- 03-backtracking.R
+++ 03-backtracking.R
@@ -1,14 +1,21 @@
 collatz_df
 
-collatz_df %>% 
-  filter(seq %in% 1)
+#testing out for loops that satisfies backtracking conditions
+for (i in 2:length(n_seq)) {
+  index <- n_seq[i]
+  if (n_seq[1] > index){
+    no <- i
+    break
+  }
+} for (j in no:length(n_seq)){
+  index2 <- n_seq[j]
+  if(n_seq[1] < index2){
+    print(n_seq)
+    break
+  }
+}
 
-view(collatz_df)
-
-result_n <- as.list(result_n)
-str
-
-typeof(result_n)
+#putting for loop in the function
 
 gen_collatz <- function(n) {
   if (n < 1) {
@@ -30,68 +37,34 @@ gen_collatz <- function(n) {
     n_seq <- c(n_seq, n)
   }
   }
-  for (i in 2:length(n_seq)){
+  for (i in 2:length(n_seq)) {
     index <- n_seq[i]
-    if (n_seq[1] > index) {
-      for (j in i+1:length(n_seq)){
-        sec_index <- n_seq[j]
-        if (n_seq[1] < sec_index){
-          return(n_seq)
-        } else {NULL}
-      }
-      
-    } else {NULL}
+    if (n_seq[1] > index){
+      no <- i
+      break
+    }
   } 
-}
-
-gen_collatz(3)
-###
-
-gen_collatz2 <- function(n) {
-  gen <- function(n) {
-    if (n %% 2 == 0){
-      return(n/2)
-    } else (n %% 2 != 0) 
-    return(3 * n + 1)
-  }
-  n_seq <- n
-  n_seq2 <- n
-  if (n < 1) {
-    stop("Input n is invalid!")
-  } else if (n == 1) {
-    n_seq <- c(1)
-  } else {
-  while (n != 1) {
-    n <- gen(n)
-    if(gen(n) > n_seq2){
-    n_seq <- c(n_seq, n)
+  for (j in no:length(n_seq)){
+    index2 <- n_seq[j]
+    if(n_seq[1] < index2){
+      print(n_seq)
+      break
     }
   }
-  return(n_seq) 
-}
 }
 
+#testing out on various n
+gen_collatz(10)
+gen_collatz(9)
+gen_collatz(4)
 gen_collatz(1)
-length(gen_collatz(5))
-gen_collatz(5)[2]
 
-n <- c(1:10000)
-
+###
+n <- c(2:10000)
 result_n <- lapply(n, gen_collatz)
-result_n2 <- lapply(n, gen_collatz2)
 
-collatz_df <- tibble(
-  start = n,
-  seq = result_n
-)
+View(result_n)
 
-collatz_df2 <- tibble(
-  start = n,
-  seq = result_n2
-)
-
-collatz_df
-view(collatz_df)
 
 um <- collatz_df %>%
   unnest(seq) 
@@ -106,30 +79,5 @@ above <- collatz_df[collatz_df$start %in% c(uni), ]
 
 unstack(rev(um))
 
-as.list(c(5, 4, 3))
 
-for (i in 2:length(n_seq)){
-  index <- n_seq[i]
-  if (n_seq[1] > index) {
-    for (j in i+1:length(n_seq)){
-      sec_index <- n_seq[j]
-      if (n_seq[1] < sec_index){
-        return(n_seq)
-      } else {NULL}
-    }
-    
-  } else {NULL}
-}
 
-for (i in 2:length(n_seq)) {
-  index <- n_seq[i]
-  if (n_seq[1] > index){
-    no <- i
-    break
-  }
-} for (j in no:length(n_seq)){
-  index2 <- n_seq[j]
-  if(n_seq[1] < index2){
-    print(n_seq)
-  }
-}
