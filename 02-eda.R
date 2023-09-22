17b4e7974c06ff84634670ed54ec01ac764ec73d
izzati-aziz <19b9044@ubd.edu.bn>
2023-09-22 20:41:58 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -8,12 +8,7 @@ library(tibble)
 
 ### 2) Exploratory data analysis
 
-#Use `{tidyverse}` data wrangling techniques to find the answers to the
-#following questions. For each question, save the R object with the
-#suggested name.
-
 #1.  Find the top 10 starting integers that produce the longest sequences
-#\[`top10longest`\].
 
 top10longest <- gen_collatz %>%
                   arrange(desc(gen_collatz$length)) %>%
@@ -24,32 +19,39 @@ top10longest <- gen_collatz %>%
 # https://statisticsglobe.com/select-top-n-highest-values-by-group-in-r
 
 #2.  Find out which starting integer produces a sequence that reaches the
-#highest maximum value \[`max_val_int`\].
+#highest maximum value 
 
 max_Val_int <- gen_collatz %>%
                 select(gen_collatz$start & gen_collatz$max_val) %>%
                 arrange(desc(gen_collatz$max_val)) %>%
+                select(gen_collatz$start(1:1))
 
 
 #3.  What is the average length and standard deviation of the sequence
 #for even starting integers compared to odd ones?
-#  \[`even_odd_avg_len` and `even_odd_sd_len`\]
-#<!-- Is there a significant^[Apply an appropriate hypothesis test and report the 
-#$p$-value.] difference? -->
   
-odd_avg_len <- gen_collatz %>%
-                select(gen_collatz$parity) %>%
-                group_by(gen_collatz$parity, Odd) %>%
-                mean()
-  
+odd <- gen_collatz %>%
+        select(gen_collatz$parity) %>%
+        group_by(Odd) %>%
+        print(gen_collatz$length)
 
+mean(odd)  
+sd(odd)
+
+even <- gen_collatz %>%
+        select(gen_collatz$parity) %>%
+        group_by(Even) %>%
+        print(gen_collatz$length)
+
+mean(even)
+sd(even)
 
 even_odd_avg_len <- 
 
-even_odd_sd_len <- gen_collatz %>%
+even_odd_sd_len <- 
                     
 # Using Shapiro Wilk test to find out the p-value
-  shapiro.test(mean = even_odd_avg_len , sd = even_odd_sd_len)
+  shapiro.test(mean = even_odd_avg_len, sd = even_odd_sd_len)
 
 # Thus producing a p-value of 
 # Since p-values is greater/less than 0.05, therefore there is a(n) (in)significant
