96903012bd156d75bba5b6024120cf7be3af86d5
izzati-aziz <19b9044@ubd.edu.bn>
2023-09-22 20:22:12 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -5,6 +5,12 @@ library(palmerpenguins)
 library(dplyr)
 library(tibble)
 
+# due to conflicted package, I ran the code to force all conflicts to become errors
+install.packages("devtools")
+devtools::install_github("r-lib/conflicted", force = TRUE)
+# ✖ dplyr::filter() masks stats::filter()
+# ✖ dplyr::lag()    masks stats::lag()
+
 ### 2) Exploratory data analysis
 
 #Use `{tidyverse}` data wrangling techniques to find the answers to the
@@ -16,7 +22,11 @@ library(tibble)
 
 top10longest <- gen_collatz %>%
                   arrange(desc(gen_collatz$length)) %>%
-                  select(gen_collatz$length(1:10))
+                  select(gen_collatz$length(1:10) & gen_collatz$start) %>%
+                  print(gen_collatz$start)
+  
+# inspired from: 
+# https://statisticsglobe.com/select-top-n-highest-values-by-group-in-r
 
 #2.  Find out which starting integer produces a sequence that reaches the
 #highest maximum value \[`max_val_int`\].
@@ -32,11 +42,21 @@ max_Val_int <- gen_collatz %>%
 #<!-- Is there a significant^[Apply an appropriate hypothesis test and report the 
 #$p$-value.] difference? -->
   
-even_odd_avg_len <- gen_collatz %>%
-                  select(gen_collatz$parity )
+odd_avg_len <- gen_collatz %>%
+                select(gen_collatz$parity) %>%
+                group_by(gen_collatz$parity, Odd) %>%
+                mean()
+  
+
+
+even_odd_avg_len <- 
 
 even_odd_sd_len <- gen_collatz %>%
                     
-# Using shapiro.wilke test to find out the p-value
-  
+# Using Shapiro Wilk test to find out the p-value
+  shapiro.test(mean = even_odd_avg_len , sd = even_odd_sd_len)
+
+# Thus producing a p-value of 
+# Since p-values is greater/less than 0.05, therefore there is a(n) (in)significant
+# difference.
   
