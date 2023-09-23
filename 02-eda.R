9d4e95dfa7926afec080b5531b3654cde45d6d92
izzati-aziz <19b9044@ubd.edu.bn>
2023-09-23 19:12:16 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -10,10 +10,10 @@ library(tibble)
 
 #1.  Find the top 10 starting integers that produce the longest sequences
 
-top10longest <- gen_collatz %>%
-                arrange(desc(gen_collatz$length), .by_group = TRUE) %>%
-                select(gen_collatz$length(1:10) & gen_collatz$start) %>%
-                print(gen_collatz$start)
+top10longest <- collatz_df %>%
+                arrange(desc(length), .by_group = TRUE) %>%
+                select(collatz_df$length(1:10) & collatz_df$start(1:10)) %>%
+                print(collatz_df$start(1:10))
   
 # inspired from: 
 # https://statisticsglobe.com/select-top-n-highest-values-by-group-in-r
@@ -21,27 +21,27 @@ top10longest <- gen_collatz %>%
 #2.  Find out which starting integer produces a sequence that reaches the
 #highest maximum value 
 
-max_Val_int <- gen_collatz %>%
-              select(gen_collatz$start & gen_collatz$max_val) %>%
-              arrange(desc(gen_collatz$max_val), .by_group = TRUE) %>%
-              select(gen_collatz$start(1:1))
+max_Val_int <- collatz_df %>%
+              select(collatz_df$start & collatz_df$max_val) %>%
+              arrange(desc(max_val), .by_group = TRUE) %>%
+              select(collatz_df$start(1:1))
 
 
 #3.  What is the average length and standard deviation of the sequence
 #for even starting integers compared to odd ones?
   
-odd <- gen_collatz %>%
-      select(gen_collatz$parity) %>%
+odd <- collatz_df %>%
+      select(parity) %>%
       group_by(Odd) %>%
-      print(gen_collatz$length)
+      print(collatz_df$length(1:n))
 
 mean(odd)  
 sd(odd)
 
-even <- gen_collatz %>%
-        select(gen_collatz$parity) %>%
+even <- collatz_df %>%
+        select(parity) %>%
         group_by(Even) %>%
-        print(gen_collatz$length)
+        print(collatz_df$length)
 
 mean(even)
 sd(even)
@@ -50,8 +50,8 @@ even_odd_avg_len <- mean(odd & even)
 
 even_odd_sd_len <- sd(odd & even)
   
-# Using Shapiro Wilk test to find out the p-value
-  shapiro.test(mean = even_odd_avg_len, sd = even_odd_sd_len)
+# Using two sample t-test to find out the p-value
+  t.test(odd, even, var.eq = FALSE)
 
 # If p-value is less than 0.05, therefore there is an insignificant difference.
 # If p-value is greater than 0.05, therefore there is a significant difference
