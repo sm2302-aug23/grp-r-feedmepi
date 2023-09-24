f43271adf1040033c20b27752417f890ca1997e7
Sia Yee Zee <19b9013@ube.edu.bn>
2023-09-24 19:25:06 +0800

    Update 04-plots.R

--- 04-plots.R
+++ 04-plots.R
@@ -4,14 +4,15 @@ library(ggplot2)
 
 
 # plot 1 draft -----------------------------------------------------------------
-
 # top 10 starting integer
 # cited from
 # https://statisticsglobe.com/select-top-n-highest-values-by-group-in-r
+
 top_10_startint <- backtracks_df %>%
   arrange(desc(length)) %>%
   group_by(length) %>%
   slice(1:10)
+
 
 # ggplot 1
 ggplot(data = backtracks_df,
@@ -28,7 +29,8 @@ ggplot(data = backtracks_df,
     x = "Starting integer",
     y = "Sequence length"
   )
-   
+
+
 #plot 2 draft ------------------------------------------------------------------
 #unique function cited from
 #https://www.geeksforgeeks.org/unique-function-in-r/
