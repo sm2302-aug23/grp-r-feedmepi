002e8948cb482335a9763096f23a8cc7817e68a7
AqilMN <19b9002@ubd.edu.bn>
2023-09-24 18:07:54 +0800

    Update 04-plots.R
    task 4 plot 2

--- 04-plots.R
+++ 04-plots.R
@@ -24,3 +24,24 @@ ggplot(data = start_freq,
     y = "Sequence length"
   )
    
+#plot 2 draft ------------------------------------------------------------------
+#unique function cited from
+#https://www.geeksforgeeks.org/unique-function-in-r/
+
+highest <- unique(backtracks_df[c("max_val")]) %>%
+  arrange(desc(max_val))
+
+top_10_highest <- backtracks_df %>% 
+  arrange(desc(max_val)) %>%
+  filter(max_val >= highest$max_val[10])
+  
+
+ggplot(data = backtracks_df,
+       mapping = aes(x = start,
+                     y = max_val)
+       ) +
+  geom_point(data = top_10_highest,
+             aes(x = start,
+                 y = max_val,
+                 col = max_val))
+
