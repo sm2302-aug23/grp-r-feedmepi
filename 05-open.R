d86b3b9558e08eeb34bc1905c060ed1c80c73fc0
izzati-aziz <19b9044@ubd.edu.bn>
2023-09-24 19:02:32 +0800

    Create 05-open.R

---
+++ 05-open.R
@@ -0,0 +1,6 @@
+## 5) Open-ended exploration
+
+# Does odd numbers produce longer sequences?
+
+# When arranged based on the length of the sequence, it is shown that 9 out of  
+# 10 of the longest sequence has odd number as the starting integer.
