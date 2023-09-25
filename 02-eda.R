6baf8af6199284bd7ac3521948e275b39719b35d
izzati-aziz <19b9044@ubd.edu.bn>
2023-09-25 22:56:24 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -25,7 +25,8 @@ top10longest <- collatz_df %>%
 max_val_int <- collatz_df %>%
                arrange(desc(max_val), .by_group = TRUE) %>%
                slice(1:1, .by = NULL, .preserve = FALSE) %>%
-               select(start)
+               select(start) %>%
+               unlist()
 
 #3.  What is the average length and standard deviation of the sequence
 #for even starting integers compared to odd ones?
