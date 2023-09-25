84b3f469cffe78651589ec4925c849a0ff3eebc2
Sia Yee Zee <19b9013@ube.edu.bn>
2023-09-26 07:59:52 +0800

    Update 06-creative.R

--- 06-creative.R
+++ 06-creative.R
@@ -3,10 +3,12 @@
 # How often specific numbers (other than 1) appear in sequences, across all starting integers
 
 # Answer
-# Numbers 2 and 4 have been found in most of the sequences, across all starting integers
+# Numbers 2, 4, 8, 16 have been found in most of the sequences, across all starting integers
 
 
 
-# finding the appearance frequencies of number 2 and 4
+# finding the appearance frequencies of these numbers
+
+
 
 
