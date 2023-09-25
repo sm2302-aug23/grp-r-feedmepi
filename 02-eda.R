6234aacf9bddbfc76f8abbade98906c862d3c752
izzati-aziz <19b9044@ubd.edu.bn>
2023-09-25 23:20:45 +0800

    Update 02-eda.R

--- 02-eda.R
+++ 02-eda.R
@@ -34,11 +34,15 @@ max_val_int <- collatz_df %>%
 
 even_odd_avg_len <- collatz_df %>%
                     group_by(parity) %>%
-                    summarise(avg = mean(length))
+                    summarise(avg = mean(length)) %>%
+                    select(avg) %>%
+                    unlist()
 
 even_odd_sd_len <- collatz_df %>%
                     group_by(parity) %>%
-                    summarise(sd = sd(length))
+                    summarise(sd = sd(length)) %>%
+                    select(sd) %>%
+                    unlist()
 
 # Using t-test to find out the p-value
   t.test(even_odd_avg_len, var.eq = FALSE)
